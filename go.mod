0836f1b250e3d6019a1431480cdebfb711b9937d
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-05-11 12:49:32 -0400

    tmini

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module github.com/machine-moon/gogo
+module projectName
 
-go 1.22.2
+go 1.23.5
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/machine-moon/gogo v0.0.0-20250107071728-ace7afbd6edd
+)
--- main.go
+++ main.go
@@ -1,8 +1,43 @@
-package main
+package main // this is similar java
 
+// similar to C / python
+
+// 2 styles can do () for multi line or manual
 import "fmt"
+import "github.com/google/uuid"
+
+//func keywordis defines a function
+// main() is reserved for entry point
+
+// use var keyword followed by variableName and TYPE
+//var myvar int = 10 // global variable
+
+//ORRRR
+
+// :=
+// u see that ':'
+// that is like python dynamic typing
+// compiler will INFER type
+// very nice syntax lolz
+//mySecondVar := 10
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	// so now lets go over this
+	// for loop
+	// see this for (init ; condition ; increment)
+
+	// SAME AS IN C BRUH
+
+	//copilot annoying lolz
+	//
+
+	// same as C right? k.
+
+	for i := 0; i < 1000; i++ {
+		// here we give the variable named uuid something
+		uuid := uuid.New()
+		fmt.Println("Generated UUID:", uuid)
+		// println == print line
+	}
 }
